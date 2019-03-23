38794cc16db918ac151860ec77a43ef1fe53319f
Conor ODonnell <c17441552@dit.ie>
2019-03-23 17:35:10 +0000

    modified:   src/ie/tudublin/Interface.java

--- java/src/ie/tudublin/Interface.java
+++ java/src/ie/tudublin/Interface.java
@@ -4,12 +4,14 @@ import processing.core.PApplet;
 
 public class Interface extends PApplet{
 
-    float x = 250;
-    float y = 250; 
+    float r;
+    float x = 0;
+    float y = 0; 
     float xspeed = 2;
     float yspeed = 1;
     float diameter = 100;
     float radius = diameter / 2;
+    float fullArc = PI + PI/1.5f;
 
 public void settings(){
     size(500,500);
@@ -26,7 +28,11 @@ public void speed(float xspeed,float yspeed){
 
 public void RotatingCircle(){
     
-    ellipse(x,y,diameter,diameter);
+    translate(width/2,height/2);
+    rotate(radians(r));
+    arc(x, y, diameter, diameter, 0, fullArc);
+    
+    r += 1;
     
 }
 
@@ -44,8 +50,8 @@ public void draw(){
     stroke(255);
     noFill();
 
-    update();
-    speed(xspeed,yspeed);
+    //update();
+    //speed(xspeed,yspeed);
     RotatingCircle();
 
 
